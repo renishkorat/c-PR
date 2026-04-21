0053250b343adde4beaf584141d137c9e859d871
Renish Korat <koratrenish350@gmail.com>
2026-04-21 10:58:07 +0530

    add

--- PR5.cpp
+++ PR5.cpp
@@ -1,128 +1,223 @@
 #include <iostream>
 using namespace std;
 
-class LibraryItem {
-private:
-    string title, author, dueDate;
+
+class LibraryItem
+{
+protected:
+    string title;
+    string author;
+    string dueDate;
 
 public:
+    LibraryItem(string t, string a, string d)
+    {
+        title = t;
+        author = a;
+        dueDate = d;
+    }
+
+    string getTitle() { return title; }
+    string getAuthor() { return author; }
+    string getDueDate() { return dueDate; }
+
     void setTitle(string t) { title = t; }
     void setAuthor(string a) { author = a; }
     void setDueDate(string d) { dueDate = d; }
 
-    string getTitle() const { return title; }
-    string getAuthor() const { return author; }
-    string getDueDate() const { return dueDate; }
-
     virtual void checkOut() = 0;
     virtual void returnItem() = 0;
-    virtual void displayDetails() const = 0;
+    virtual void displayDetails() = 0;
 
     virtual ~LibraryItem() {}
 };
 
-class Book : public LibraryItem {
+class Book : public LibraryItem
+{
 public:
-    void checkOut() override { setDueDate("15 Days"); }
-    void returnItem() override { setDueDate("Returned"); }
-    void displayDetails() const override {
-        cout << "Book | Title: " << getTitle()
-             << " | Author: " << getAuthor()
-             << " | Status: " << getDueDate() << endl;
+    Book(string t, string a, string d) : LibraryItem(t, a, d) {}
+
+    void checkOut() override
+    {
+        cout << "Book Checked Out: " << title << endl;
+    }
+
+    void returnItem() override
+    {
+        cout << "Book Returned: " << title << endl;
+    }
+
+    void displayDetails() override
+    {
+        cout << "\n[Book]"
+             << "\nTitle: " << title
+             << "\nAuthor: " << author
+             << "\nDue Date: " << dueDate << endl;
     }
 };
 
-class DVD : public LibraryItem {
+class DVD : public LibraryItem
+{
+    int duration;
+
 public:
-    void checkOut() override { setDueDate("7 Days"); }
-    void returnItem() override { setDueDate("Returned"); }
-    void displayDetails() const override {
-        cout << "DVD  | Title: " << getTitle()
-             << " | Author: " << getAuthor()
-             << " | Status: " << getDueDate() << endl;
+    DVD(string t, string a, string d, int dur)
+        : LibraryItem(t, a, d)
+    {
+        duration = dur;
+    }
+
+    void checkOut() override
+    {
+        cout << "DVD Checked Out: " << title << endl;
+    }
+
+    void returnItem() override
+    {
+        cout << "DVD Returned: " << title << endl;
+    }
+
+    void displayDetails() override
+    {
+        cout << "\n[DVD]"
+             << "\nTitle: " << title
+             << "\nAuthor: " << author
+             << "\nDue Date: " << dueDate
+             << "\nDuration: " << duration << " mins" << endl;
     }
 };
 
-class Magazine : public LibraryItem {
+class Magazine : public LibraryItem
+{
+    int issueNumber;
+
 public:
-    void checkOut() override { setDueDate("30 Days"); }
-    void returnItem() override { setDueDate("Returned"); }
-    void displayDetails() const override {
-        cout << "Magazine | Title: " << getTitle()
-             << " | Author: " << getAuthor()
-             << " | Status: " << getDueDate() << endl;
+    Magazine(string t, string a, string d, int issue)
+        : LibraryItem(t, a, d)
+    {
+        issueNumber = issue;
+    }
+
+    void checkOut() override
+    {
+        cout << "Magazine Checked Out: " << title << endl;
+    }
+
+    void returnItem() override
+    {
+        cout << "Magazine Returned: " << title << endl;
+    }
+
+    void displayDetails() override
+    {
+        cout << "\n[Magazine]"
+             << "\nTitle: " << title
+             << "\nAuthor: " << author
+             << "\nDue Date: " << dueDate
+             << "\nIssue No: " << issueNumber << endl;
     }
 };
 
-int main() {
-    const int MAX = 10;
-    LibraryItem* items[MAX];
-    int count = 0;
+int main()
+{
+    const int MAX=10;
+LibraryItem* items[MAX];
+int count = 0;
+
     int choice;
 
-    do {
-        cout << "\n------ Library Menu ------\n";
-        cout << "1. Add Book\n2. Add DVD\n3. Add Magazine\n";
-        cout << "4. Check Out Item\n5. Return Item\n6. Display All Items\n7. Exit\n";
-        cout << "Enter choice: ";
+    do
+    {
+        cout << "\n===== Library Menu =====";
+        cout << "\n1. Add Book";
+        cout << "\n2. Add DVD";
+        cout << "\n3. Add Magazine";
+        cout << "\n4. Display All";
+        cout << "\n5. Checkout Item";
+        cout << "\n6. Return Item";
+        cout << "\n0. Exit";
+        cout << "\nEnter choice: ";
         cin >> choice;
 
-        try {
-            if ((choice == 1 || choice == 2 || choice == 3) && count < MAX) {
-                LibraryItem* item = nullptr;
-                if (choice == 1) item = new Book;
-                if (choice == 2) item = new DVD;
-                if (choice == 3) item = new Magazine;
+        try
+        {
+            if (choice == 1)
+            {
+                string t, a, d;
+                cout << "Enter title: ";
+                cin >> t;
+                cout << "Enter author: ";
+                cin >> a;
+                cout << "Enter due date: ";
+                cin >> d;
 
-                string t, a;
-                cout << "Enter Title: "; cin >> t;
-                cout << "Enter Author: "; cin >> a;
-                item->setTitle(t);
-                item->setAuthor(a);
-                item->setDueDate("Available");
-                items[count++] = item;
+                items[count++] = new Book(t, a, d);
             }
-         else if (choice == 4) {
-    int i;
-    cout << "Enter item index: ";
-    cin >> i;
+            else if (choice == 2)
+            {
+                string t, a, d;
+                int dur;
 
-    if (i >= 0 && i < count) {
-        items[i]->checkOut();
-        cout << "Item Checked Out!" << endl;
-    }
-    else {
-        cout << "Invalid Index!" << endl;
-    }
-}
-            else if (choice == 5) {
-    int i;
-    cout << "Enter item index: ";
-    cin >> i;
+                cout << "Enter title: ";
+                cin >> t;
+                cout << "Enter author: ";
+                cin >> a;
+                cout << "Enter due date: ";
+                cin >> d;
+                cout << "Enter duration: ";
+                cin >> dur;
 
-    if (i >= 0 && i < count) {
-        items[i]->returnItem();
-        cout << "Item Returned Successfully!" << endl;
-    }
-    else {
-        cout << "Invalid Index!" << endl;
-    }
-}
-           else if (choice == 6) {
-    for (int i = 0; i < count; i++) {
-        cout << "Index: " << i << " -> ";
-        items[i]->displayDetails();
-    }
-}
+                if (dur < 0)
+                    throw "Invalid duration!";
+
+                items[count++] = new DVD(t, a, d, dur);
+            }
+            else if (choice == 3)
+            {
+                string t, a, d;
+                int issue;
+
+                cout << "Enter title: ";
+                cin >> t;
+                cout << "Enter author: ";
+                cin >> a;
+                cout << "Enter due date: ";
+                cin >> d;
+                cout << "Enter issue number: ";
+                cin >> issue;
+
+                if (issue < 0)
+                    throw "Invalid issue number!";
+
+                items[count++] = new Magazine(t, a, d, issue);
+            }
+            else if (choice == 4)
+            {
+                for (int i = 0; i < count; i++)
+                    items[i]->displayDetails();
+            }
+            else if (choice == 5)
+            {
+                int i;
+                cout << "Enter index: ";
+                cin >> i;
+                items[i]->checkOut();
+            }
+            else if (choice == 6)
+            {
+                int i;
+                cout << "Enter index: ";
+                cin >> i;
+                items[i]->returnItem();
+            }
+
         }
-        catch (...) {
-            cout << "\nError: Invalid Input!\n";
+        catch (const char* msg)
+        {
+            cout << "Error: " << msg << endl;
         }
 
-    } while (choice != 7);
-
-    for (int i = 0; i < count; i++)
-        delete items[i];
+    } while (choice != 0);
 
     return 0;
 }
